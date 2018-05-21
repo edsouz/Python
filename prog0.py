23f29291c4bc23bff5c8eefd1a793265279e3cae
Edson <edsouz@gmail.com>
2018-05-21 11:01:28 -0300

    Progrma mais complexo

--- prog0.py
+++ prog0.py
@@ -1,2 +1,20 @@
 a = int(input("Digite um número inteiro: "))
-print("o número digitado foi %d" %a)
+b = int(input("Digite um outro número inteiro: "))
+
+def soma(x,y):
+    return x + y
+
+s = soma(a,b)
+
+print(" ")
+print(" ======================================== ")
+print("o primeiro número digitado foi %d\n" %a)
+print("O segundo número digitado foi %d\n" %b)
+print("A soma dos números digitados é %d" %s)
+print(" ======================================== ")
+print(" ")
+
+
+
+
+
