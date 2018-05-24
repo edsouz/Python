132aee014838a0679be19073491b72ee510caae4
Edson <edsouz@gmail.com>
2018-05-24 10:18:38 -0300

    Dicionários em python

---
+++ dicionários.py
@@ -0,0 +1,5 @@
+dicionário = {'chave1' : 123, 'chave2' : 'string', 'chave3' : [1, 2, 3]}
+print(type(dicionário))
+
+print(dicionário['chave3'])
+print(dicionário['chave3'][2])
--- listas.py
+++ listas.py
@@ -9,4 +9,33 @@ for i in lista1:
     print(i, '\n')
 
 
+lista1.append(7)
+lista2.append(10)
+
+print(lista1)
+print(lista2)
+
+print("\n")
+
+lista1.pop()
+lista2.pop(1)
+
+print(lista1, '\n')
+print(lista2, '\n')
+
+lista1.reverse()
+print(lista1, '\n')
+
+lista1.sort()
+print(lista1, '\n')
+
+x = lista1.index(3)
+
+print('O índice do item 3 é ', x, '\n')
+
+lista2.remove(7)
+
+print(lista2)
+
+
 
