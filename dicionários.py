af778955772e47d808ab17af07e1a5dfaf0557b3
Edson <edsouz@gmail.com>
2018-05-30 09:35:48 -0300

    set

--- dicionários.py
+++ dicionários.py
@@ -3,3 +3,8 @@ print(type(dicionário))
 
 print(dicionário['chave3'])
 print(dicionário['chave3'][2])
+print(dicionário['chave2'].upper())
+print(dicionário)
+
+dicionário['chave1'] = dicionário['chave1'] + 4
+print(dicionário)
